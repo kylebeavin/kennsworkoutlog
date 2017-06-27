188da70d78e0ed04c35e4032c508e846339964fd
Kenn Pascascio <kenn370@gmail.com>
2017-06-27 16:16:56 -0400

    added db connection

--- app.js
+++ app.js
@@ -1,5 +1,54 @@
 var express = require('express');
 var app = express();
+var Sequelize = require('sequelize');
+var bodyParser = require('body-parser');
+
+var seq = new Sequelize('workoutlog', 'postgres', 'k12323',{
+	host:'localhost',
+	dialect: 'postgres'
+});
+
+seq.authenticate().then(
+	function(){
+		console.log("you're connected to the pg database")
+	},
+	function(err){
+		console.log(err)
+	}
+);
+
+var User = seq.define('user', {
+	username: Sequelize.STRING,
+	passwordhash: Sequelize.STRING 
+})
+
+User.sync()
+app.use(bodyParser.json());
+
+
+app.post('/api/user', function(req, res){
+	var username = req.body.user.username
+	var pass = req.body.user.password
+
+
+	User.create({
+		username : username,
+		passwordhash :""
+	}).then(
+		function createSuccess(user){
+			res.json({
+				user : user,
+				message: 'create'
+			})
+		},
+		function createError(err){
+			res.send(500, err.message)
+		}
+
+		)
+
+})
+
 
 app.use(require('./middleware/headers'));
 

5ae64740af6bb3c1de6ee37dd083313ed802f191
Kenn Pascascio <kenn370@gmail.com>
2017-06-27 13:57:57 -0400

    Added middleware for our header

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use(require('./middleware/headers'));
+
 app.use('/api/test', function(req, res){
 	res.send("<h1>Hello World</h1>" +
 			"<h2> it's a small world</h2>");

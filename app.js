d890d0696dae93cff211f836a498093a49c5b770
Kenn Pascascio <kenn370@gmail.com>
2017-06-27 11:28:01 -0400

    fixed the app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use('/api/test', function(req, res){
+	res.send("<h1>Hello World</h1>" +
+			"<h2> it's a small world</h2>");
+})
 
 app.listen(3000, function(){
 	console.log("app is open on 3000!");

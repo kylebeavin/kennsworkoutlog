6cb54a6e623773b72e1826c06db9a15534995682
Kenn Pascascio <kenn370@gmail.com>
2017-06-28 09:29:51 -0400

    bcrypt

--- app.js
+++ app.js
@@ -4,37 +4,14 @@ var bodyParser = require('body-parser');
 var seq = require('./db.js');
 var User = seq.import('./models/user');
 
-
 User.sync()
+
 app.use(bodyParser.json());
-
-
-app.post('/api/user', function(req, res){
-	var username = req.body.user.username
-	var pass = req.body.user.password
-
-
-	User.create({
-		username : username,
-		passwordhash :""
-	}).then(
-		function createSuccess(user){
-			res.json({
-				user : user,
-				message: 'create'
-			})
-		},
-		function createError(err){
-			res.send(500, err.message)
-		}
-
-		)
-
-})
-
 
 app.use(require('./middleware/headers'));
 
+app.use('/api/user', require("./routes/user"))
+app.use('/api/login', require('./routes/session'))
 app.use('/api/test', function(req, res){
 	res.send("<h1>Hello World</h1>" +
 			"<h2> it's a small world</h2>");
